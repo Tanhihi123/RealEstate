4ce007a936fc036572699a8f641a1e8bf4ff1d6d
Tanhihi123 <tandiscord2002@gmail.com>
2023-10-12 17:45:25 +0700

    UI_dashboard_property

---
+++ src/components/icons/index.tsx
@@ -0,0 +1,11 @@
+export { default as IconSearch } from "./IconSearch";
+export { default as IconNotification } from "./IconNotification";
+export { default as IconDashboard } from "./IconDashboard";
+export { default as IconBuilding } from "./IconBuilding";
+export { default as IconStar } from "./IconStar";
+export { default as IconMessage } from "./IconMessage";
+export { default as IconProfile } from "./IconProfile";
+export { default as IconPerson } from "./IconPerson";
+export { default as IconLocation } from "./IconLocation";
+export { default as IconBeds } from "./IconBeds";
+export { default as IconCross } from "./IconCross";
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,20 @@
-import type { Config } from 'tailwindcss'
-
-const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        grayf4: "#F4F4F4",
+        grayfc: "#FCFCFC",
+        gray80: "#808191",
+        primaryText: "#11142D",
+        primary: "#475BE8",
+        secondary: "#DADEFA",
+      },
+      spacing: {
+        c10: "10px",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
